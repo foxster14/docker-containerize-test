f017d8f362946e7f9ecaf9357336cbd431d7888e
sarahfox <sarah.fox.h@gmail.com>
2021-05-21 13:12:46 -0500

    made changes to loggedin.js logic

--- routes/loggedin.js
+++ routes/loggedin.js
@@ -7,33 +7,31 @@ var userData = require('../users.json');
 /* GET users listing. */
 router.post('/', function(req, res, next) {
 
-    //let userData = fs.readFileSync('./users.json');
-    //let siteUsers = JSON.parse(userData);
-
     var emailUserEntered = req.body.email; // email user entered
     var passwordUserEntered = req.body.password; //password user entered
 
     console.log(emailUserEntered);
+    console.log(passwordUserEntered);
+
+    /*if (userData.filter(x => x.email === emailUserEntered && x.password == passwordUserEntered)){
+        res.render('loggedin', { email: String(emailUserEntered)})
+        console.log("success");
+    }*/
 
     for(var i = 0; i < userData.length; i++){
 
         if(userData[i].email === emailUserEntered && userData[i].password == passwordUserEntered) {
             res.render('loggedin', { email: String(emailUserEntered)})
-            break;
+            //break;
         }
         else{
             console.log("error");
-            break;
+            res.redirect(req.get('referer'));
+            //break;
         }
 
     }
 
-
-    /*let userPassword = req.body.password;
-    let userEmail = req.body.email;
-
-    var result1 = userData.filter(x => x.password === userPassword);
-    var result2 = userData.filter(x => x.email == userEmail);*/
 
     
 });
